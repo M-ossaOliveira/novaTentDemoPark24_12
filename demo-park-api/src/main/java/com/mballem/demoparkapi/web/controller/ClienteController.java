81bf241ae996a241748f8d20c9c47d4a4ea0bed3
M-OssaOliveira <molekamari@gmail.com>
2025-12-26 17:56:19 -0300

    teste ponto a ponto para Create (Status 201)

--- demo-park-api/src/main/java/com/mballem/demoparkapi/web/controller/ClienteController.java
+++ demo-park-api/src/main/java/com/mballem/demoparkapi/web/controller/ClienteController.java
@@ -8,6 +8,10 @@ import com.mballem.demoparkapi.service.UsuarioService;
 import com.mballem.demoparkapi.web.dto.ClienteCreateDto;
 import com.mballem.demoparkapi.web.dto.ClienteResponseDto;
 import com.mballem.demoparkapi.web.dto.mapper.ClienteMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -23,12 +27,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/clientes")
+
+
 public class ClienteController {
     private final ClienteService clienteService;
     private final UsuarioService usuarioService;
 
     @PostMapping
     @PreAuthorize("hasRole('CLIENTE')")
+    @Operation(summary = "Criar novo cliente",
+            description = "Recurso para criar um novo cliente vinculado a um usuário cadastrado"
+    +"Requisição exige o uso de um bearer token. Acesso restrito ao Role='CLIENTE'",
+    responses = {
+            @ApiResponse(responseCode = "201",description = "Recurso criado com sucesso",
+                    content = @Content(mediaType = "application/json;charset=UTF-8",
+                            schema = @Schema(implementation = ClienteResponseDto.class))),
+            @ApiResponse(responseCode ="409" ,description = "Cliente CPF já possui cadastro no sistema",
+                    content = @Content(mediaType = "application/json;charset=UTF-8",
+                            schema = @Schema(implementation = ClienteResponseDto.class))),
+            @ApiResponse(responseCode = "422",description = "Recurso não processados por falta de dados OR dados inválidos",
+                    content = @Content(mediaType = "application/json;charset=UTF-8",
+                            schema = @Schema(implementation = ClienteResponseDto.class))),
+            @ApiResponse(responseCode = "403",description = "Recurso NÃO permitido para perfil do tipo 'ADMIN'",
+                    content = @Content(mediaType = "application/json;charset=UTF-8",
+                            schema = @Schema(implementation = ClienteResponseDto.class)))
+    })
+
     public ResponseEntity<ClienteResponseDto> create(@RequestBody @Valid ClienteCreateDto dto,
                                                      @AuthenticationPrincipal JwtUserDetails userDetails) {
         Cliente cliente = ClienteMapper.toCliente(dto);
